a8c68b592f9d0e60e0cbf83b65cd8e22042399a5
ysmgthntt <17098124+ysmgthntt@users.noreply.github.com>
2023-12-06 13:44:36 +0900

    QueryObjects .NET 3.5 対応

--- src/QueryObjects/QueryCommon.cs
+++ src/QueryObjects/QueryCommon.cs
@@ -1,16 +1,24 @@
-﻿using System.Buffers;
-
-namespace GyoumuLib.QueryObjects
+﻿namespace GyoumuLib.QueryObjects
 {
     internal static class QueryCommon
     {
-        private static readonly SearchValues<char> _invalidChars = SearchValues.Create(". +-*/,\"'");
+        private const string INVALID_CHARS = ". +-*/,\"'";
+
+#if NET8_0_OR_GREATER
+        private static readonly System.Buffers.SearchValues<char> _invalidChars = System.Buffers.SearchValues.Create(INVALID_CHARS);
+#else
+        private static readonly char[] _invalidChars = INVALID_CHARS.ToArray();
+#endif
 
         public static void ValidateColumnName(string columnName, string paramName = "columnName")
         {
             ANE.ThrowIfNullOrEmpty(columnName, paramName);
 
+#if NET8_0_OR_GREATER
             var s = columnName.AsSpan();
+#else
+            var s = columnName;
+#endif
             var found = s.IndexOfAny(_invalidChars);
             if (found >= 0)
                 throw new ArgumentException($"Invalid column name. Invalid char {s[found]}", paramName);
